83305f15dc6109d146ccd541be63a55d7df76af4
Canutilho <adcanutilho@estudante.ufscar.br>
2025-04-23 10:37:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
+import pandas as pd
 
 st.title('⚗️ Ethanol AI')
 
 st.info('Aqui é possível prever as melhores condições para produzir o seu etanol de segunda geração.')
-
-st.write('Hello world!')
