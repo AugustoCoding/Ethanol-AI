564037c192b1a5e56b82ef6725f67098f09f050b
Canutilho <adcanutilho@estudante.ufscar.br>
2025-04-29 15:23:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,11 +28,28 @@ cinzas = st.sidebar.number_input("Porcentagem de cinzas", min_value=0.0, max_val
 st.sidebar.header("Parâmetos do modelo")
 
 # Se a biomassa escolhida foi Bagaço de Cana-de-Açúcar
-
 if biomassa == "Bagaço de Cana-de-Açúcar":
-  st.sidebar.number_input("Parâmetro 1")
-  st.sidebar.number_input("Parâmetro 2")
-  st.sidebar.number_input("Parâmetro 3")
-  st.sidebar.selectbox("Parâmetro 4", ['Opção 1', 'Opção 2', 'Opção 3'])
-  st.sidebar.selectbox("Parâmetro 5", ['Opção 1', 'Opção 2', 'Opção 3'])
-  st.sidebar.selectbox("Parâmetro 6", ['Opção 1', 'Opção 2', 'Opção 3'])
+    bagaco1 = st.sidebar.number_input("Parâmetro 1 - Bagaço")
+    bagaco2 = st.sidebar.number_input("Parâmetro 2 - Bagaço")
+    bagaco3 = st.sidebar.number_input("Parâmetro 3 - Bagaço")
+    bagaco4 = st.sidebar.selectbox("Parâmetro 4 - Bagaço", options=["Opção 1", "Opção 2", "Opção 3"])
+    bagaco5 = st.sidebar.selectbox("Parâmetro 5 - Bagaço", options=["Opção A", "Opção B", "Opção C"])
+    bagaco6 = st.sidebar.selectbox("Parâmetro 6 - Bagaço", options=["Sim", "Não"])
+
+# Se a biomassa escolhida foi Palha da Cana-de-Açúcar
+elif biomassa == "Palha da Cana-de-Açúcar":
+    palha1 = st.sidebar.number_input("Parâmetro 1 - Palha Cana")
+    palha2 = st.sidebar.number_input("Parâmetro 2 - Palha Cana")
+    palha3 = st.sidebar.number_input("Parâmetro 3 - Palha Cana")
+    palha4 = st.sidebar.selectbox("Parâmetro 4 - Palha Cana", options=["Opção 1", "Opção 2", "Opção 3"])
+    palha5 = st.sidebar.selectbox("Parâmetro 5 - Palha Cana", options=["Opção A", "Opção B", "Opção C"])
+    palha6 = st.sidebar.selectbox("Parâmetro 6 - Palha Cana", options=["Sim", "Não"])
+
+# Se a biomassa escolhida foi Palha de Milho
+elif biomassa == "Palha de milho":
+    milho1 = st.sidebar.number_input("Parâmetro 1 - Milho")
+    milho2 = st.sidebar.number_input("Parâmetro 2 - Milho")
+    milho3 = st.sidebar.number_input("Parâmetro 3 - Milho")
+    milho4 = st.sidebar.selectbox("Parâmetro 4 - Milho", options=["Opção 1", "Opção 2", "Opção 3"])
+    milho5 = st.sidebar.selectbox("Parâmetro 5 - Milho", options=["Opção A", "Opção B", "Opção C"])
+    milho6 = st.sidebar.selectbox("Parâmetro 6 - Milho", options=["Sim", "Não"])
