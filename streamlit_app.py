d6f0a3b2389cfe76c71d5b5700f11993b8b19aec
Canutilho <adcanutilho@estudante.ufscar.br>
2025-04-29 15:31:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,17 @@ lignina = st.sidebar.number_input("Porcentagem de Lignina", min_value=0.0, max_v
 hemicelulose = st.sidebar.number_input("Porcentagem de Hemicelulose", min_value=0.0, max_value=100.0, format="%.2f")
 cinzas = st.sidebar.number_input("Porcentagem de cinzas", min_value=0.0, max_value=100.0, format="%.2f")
 
-#Apresentar os campos de input corretos para cada biomassa escolhida
-
-st.sidebar.header("Parâmetos do modelo")
+st.header("Parâmetros")
+st.write(f"Selecione os parâmetros para definir a sua condição de operação para {biomassa}.")
 
 # Se a biomassa escolhida foi Bagaço de Cana-de-Açúcar
 if biomassa == "Bagaço de Cana-de-Açúcar":
-    bagaco1 = st.sidebar.number_input("Parâmetro 1 - Bagaço")
-    bagaco2 = st.sidebar.number_input("Parâmetro 2 - Bagaço")
-    bagaco3 = st.sidebar.number_input("Parâmetro 3 - Bagaço")
-    bagaco4 = st.sidebar.selectbox("Parâmetro 4 - Bagaço", options=["Opção 1", "Opção 2", "Opção 3"])
-    bagaco5 = st.sidebar.selectbox("Parâmetro 5 - Bagaço", options=["Opção A", "Opção B", "Opção C"])
-    bagaco6 = st.sidebar.selectbox("Parâmetro 6 - Bagaço", options=["Sim", "Não"])
+    bagaco1 = st.number_input("Parâmetro 1 - Bagaço")
+    bagaco2 = st.number_input("Parâmetro 2 - Bagaço")
+    bagaco3 = st.number_input("Parâmetro 3 - Bagaço")
+    bagaco4 = st.selectbox("Parâmetro 4 - Bagaço", options=["Opção 1", "Opção 2", "Opção 3"])
+    bagaco5 = st.selectbox("Parâmetro 5 - Bagaço", options=["Opção A", "Opção B", "Opção C"])
+    bagaco6 = st.selectbox("Parâmetro 6 - Bagaço", options=["Sim", "Não"])
 
 # Se a biomassa escolhida foi Palha da Cana-de-Açúcar
 elif biomassa == "Palha da Cana-de-Açúcar":
