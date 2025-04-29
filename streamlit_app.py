2b8e5be88b42c5d9129af3be20892ac9b4e2d059
Canutilho <adcanutilho@estudante.ufscar.br>
2025-04-29 14:34:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Título do app
-st.title('⚗️ Ethanol AI')
+st.title('⚗️Ethanol AI')
 
 # Informação principal
 st.info('Aqui é possível prever as melhores condições para produzir o seu etanol de segunda geração.')
@@ -12,9 +12,22 @@ st.sidebar.title("Parâmetros")
 st.sidebar.write("Ajuste os parâmetros do modelo.")
 
 # Escolha da biomassa
+
 biomassa = st.sidebar.selectbox('Biomassa', ['Bagaço de Cana-de-Açúcar', 'Palha da Cana-de-Açúcar', 'Palha de milho'])
 st.write(f"Você escolheu: {biomassa}")
 
+
+#Ajustando a composição da biomassa
+
+st.sidebar.header('Composição')
+st.sidebar.number_input("Porcentagem de Celulose")
+st.sidebar.number_input("Porcentagem de Lignina")
+st.sidebar.number_input("Porcentagem de Hemicelulose")
+
+#Apresentar os campos de input corretos para cada biomassa escolhida
+
+#if biomassa = "Bagaço de Cana-de-Açúcar"
+  
 # Exemplo de campo de input
 valor_input = st.sidebar.number_input("Concentração de Enzimas")
 st.write(f"Concentração de Enzimas: {valor_input}")
