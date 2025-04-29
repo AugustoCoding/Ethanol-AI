6667ab8b978a506cc4ee5a156f3ddd03ee679e4e
Canutilho <adcanutilho@estudante.ufscar.br>
2025-04-29 16:46:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ with col1:
         milho4 = st.selectbox("Parâmetro 4 - Milho", options=["Opção 1", "Opção 2", "Opção 3"])
         milho5 = st.selectbox("Parâmetro 5 - Milho", options=["Opção A", "Opção B", "Opção C"])
         milho6 = st.selectbox("Parâmetro 6 - Milho", options=["Sim", "Não"])
-
+st.markdown("<hr style='border: 1px solid #ccc;' />", unsafe_allow_html=True)
 # Personalizando a coluna Resultados (col2)
 from plotly import graph_objs as go
 with col2:
